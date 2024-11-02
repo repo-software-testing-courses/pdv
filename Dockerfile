22a8ca72d1b258e18b094e8a70ed8d080078d6ab
Clara Lino <clara.lino@bravaenergia.com>
2024-11-02 15:58:34 -0300

    primeiro teste clara

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM openjdk:8-jdk-slim
 RUN apt-get update && apt-get install -y maven
 
 WORKDIR /app
+
---
+++ src/test/java/net/originmobi/pdv/service/AjusteProdutoServiceTest.java
@@ -0,0 +1,30 @@
+package net.originmobi.pdv.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.originmobi.pdv.enumerado.ajuste.AjusteStatus;
+import net.originmobi.pdv.model.Ajuste;
+import net.originmobi.pdv.model.AjusteProduto;
+import net.originmobi.pdv.model.Produto;
+import net.originmobi.pdv.repository.AjusteProdutoRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+public class AjusteProdutoServiceTest {
+    
+}
